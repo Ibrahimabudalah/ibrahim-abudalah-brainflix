2e533f7de26a7ae82494240ece6995ed1cd9bdea
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-02-27 02:17:41 -0800

    updated nav styles for all screen sizes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,13 @@ import profilePic from "../../assets/images/Mohan-muruge.jpg";
 
 function Header() {
   return (
-    <header>
-      <img src={logo} alt="BrainFlix logo" className="logo" />
-      <form className="form">
-        <div className="form__container">
-          <input type="text" placeholder="Search" className="form__container--input" />
-          <img
-            src={profilePic}
-            alt="Profile Avatar"
-            className="form__container--picture"/>
-        </div>
-        <button className="form__button">UPLOAD</button>
-      </form>
+    <header className="header">
+      <img src={logo} alt="BrainFlix logo" className="header__logo" />
+      <div className="header__container">
+        <input type="text" placeholder="Search" className="header__container__input" />
+        <img src= {profilePic} alt="Profile Avatar" className="header__container__picture"/>
+      <button className="header__button">UPLOAD</button>
+      </div>
     </header>
   );
 }
