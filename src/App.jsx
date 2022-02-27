1f1bbe87456c1743d3a69d4fe9c49543718697ad
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-02-27 04:21:59 -0800

    changed from regular function to a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import './App.scss';
+import React from 'react';
+import "./App.scss";
 import Header from "./components/Header/Header";
 import Video from "./components/HeroVideo/MainVideo";
-import videos from '../src/data/video-details.json';
-import VideoInfo from './components/VideoInfo/VideoInfo';
-import VideoDescription from './components/VideoDescription/VideoDescription';
+import videos from "../src/data/video-details.json";
+import VideoInfo from "./components/VideoInfo/VideoInfo";
+import VideoDescription from "./components/VideoDescription/VideoDescription";
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <Video videos={videos} />
-        <VideoInfo info={videos} />
-        <VideoDescription discription={videos} />
-      </header>
-    </div>
-  );
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <Video videos={videos} />
+          <VideoInfo info={videos} />
+          <VideoDescription discription={videos} />
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
