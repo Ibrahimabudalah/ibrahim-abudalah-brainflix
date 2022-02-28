8d2f01754af6390356a53566a73d27ce966a034e
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-02-27 19:28:09 -0800

    styled the list videos for  mobile view

--- src/components/SideVideo/SideVideo.jsx
+++ src/components/SideVideo/SideVideo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SideVideo.scss";
 
-function SideVideo({video, eventHandler}) {
+function SideVideo({ video, eventHandler }) {
   console.log("SideVideo video", video);
   console.log("sidevideo eventHandler", eventHandler);
   const clickHandler = (e, id) => {
@@ -13,10 +13,20 @@ function SideVideo({video, eventHandler}) {
         clickHandler(e, video.id);
       }}
       id={video.id}
+      className="videos"
     >
-      <img src={video.image} alt={video.title} />
-      <h3>{video.title}</h3>
-      <p>{video.channel}</p>
+      <div className="videos__container">
+        <img
+          src={video.image}
+          alt={video.title}
+          className="videos__container__image"
+        />
+
+        <div className="videos__container__info">
+          <h3 className="videos__container__info__title">{video.title}</h3>
+          <p className="videos__container__info__channel">{video.channel}</p>
+        </div>
+      </div>
     </div>
   );
 }
