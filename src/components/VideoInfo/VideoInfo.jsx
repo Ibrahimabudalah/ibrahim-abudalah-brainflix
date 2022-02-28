032edc101e336f17cffd06c6b1ee5e47e89746da
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-02-27 20:32:11 -0800

    styled the video info section for the mobile screen

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -9,12 +9,20 @@ function VideoInfo(props) {
     <div className="video">
       <h1 className="video__title">{props.videos.title}</h1>
       <div className="video__info">
-        <p>By {props.videos.channel}</p>
-        <img src={views} alt="views" />
-        <p>{props.videos.views}</p>
-        <p>{videoDate.toLocaleDateString("en-US")}</p>
-        <img src={likes} alt="likes" />
-        <p>{props.videos.likes}</p>
+        <div className="video__info__container">
+          <p className="video__info__channel">By {props.videos.channel}</p>
+          <div className="video__info__container__views">
+            <img src={views} alt="views" />
+            <p>{props.videos.views}</p>
+          </div>
+        </div>
+        <div className="video__info__container--margin">
+          <p>{videoDate.toLocaleDateString("en-US")}</p>
+          <div className="video__info__container__likes">
+            <img src={likes} alt="likes" />
+            <p>{props.videos.likes}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
