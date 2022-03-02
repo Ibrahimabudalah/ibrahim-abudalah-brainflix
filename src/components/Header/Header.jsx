dce3a54f2754dca1c9dde2766258f9b6fa575231
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-02 14:51:54 -0800

    linked the nav logo to the Home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import "./Header.scss";
 import logo from "../../assets/logos/BrainFlix-logo.svg";
 import profilePic from "../../assets/images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="BrainFlix logo" className="header__logo" />
+      <Link to= "/">
+        <img src={logo} alt="BrainFlix logo" className="header__logo" />
+      </Link>
+
       <div className="header__container">
-        <input type="text" placeholder="Search" className="header__container__input" />
-        <img src= {profilePic} alt="Profile Avatar" className="header__container__picture"/>
-      <button className="header__button">UPLOAD</button>
+        <input
+          type="text"
+          placeholder="Search"
+          className="header__container__input"
+        />
+        <img
+          src={profilePic}
+          alt="Profile Avatar"
+          className="header__container__picture"
+        />
+        <button className="header__button">UPLOAD</button>
       </div>
     </header>
   );
