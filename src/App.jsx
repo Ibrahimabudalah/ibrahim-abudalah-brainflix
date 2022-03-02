8b616fb288aeb4e3028813293f4145c605eb8222
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-02 12:53:58 -0800

    created the home component and imported all the components used in the home page and imported the home component in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
-import MainVideo from "./components/MainVideo/MainVideo";
-import videoDetails from "../src/data/video-details.json";
-import videosList from "../src/data/videos.json";
-import VideoInfo from "./components/VideoInfo/VideoInfo";
-import VideoDescription from "./components/VideoDescription/VideoDescription";
-import SideVideos from "./components/SideVideo/SideVideo";
-import SideVideosList from "./components/SideVideoList/SideVideoList";
-import CommentsForm from "./components/CommentsForm/CommentsForm";
-import Comment from "./components/Comment/Comment";
-import CommentsList from "./components/CommentsList/CommentsList";
+import Home from "./components/Home/Home";
+
 
 class App extends React.Component {
-  state = {
-    videoDetails: videoDetails[0],
-    videoList: videosList,
-  };
-
-  updateSelectedVideo = (id) => {
-     let selectedVideo = videoDetails.find(video => video.id === id);
-
-     this.setState({
-       videoDetails: selectedVideo,
-     });
-  };
-
   render() {
-    const filteredVideos = this.state.videoList.filter(video => video.id !== this.state.videoDetails.id)
-    // console.log("app filter videos", filteredVideos)
     return (
       <div className="App">
-        <header className="App-header">
           <Header />
-          <MainVideo videos={this.state.videoDetails} />
-          <VideoInfo videos={this.state.videoDetails} />
-          <VideoDescription videos={this.state.videoDetails} />
-          <CommentsForm comments={this.state.videoDetails.comments} />
-          {/* <Comment comments={this.state.videoDetails} /> */}
-          <CommentsList comments={this.state.videoDetails.comments} />
-          
-          {/* <SideVideos
-            sideVideos={this.selectedVideo}
-            eventHandler={this.updateSelectedVideo}
-          /> */}
-          <SideVideosList
-            list={filteredVideos}
-            eventHandler={this.updateSelectedVideo}
-          />
-        </header>
+          <Home />
       </div>
     );
   }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import MainVideo from "../MainVideo/MainVideo";
+import VideoInfo from "../VideoInfo/VideoInfo";
+import VideoDescription from "../VideoDescription/VideoDescription";
+import CommentsForm from "../CommentsForm/CommentsForm";
+import CommentsList from "../CommentsList/CommentsList";
+import SideVideosList from "../SideVideoList/SideVideoList";
+import videoDetails from "../../data/video-details.json";
+import videosList from "../../data/videos.json";
+
+class Home extends React.Component {
+  state = {
+    videoDetails: videoDetails[0],
+    videoList: videosList,
+  };
+
+  updateSelectedVideo = (id) => {
+    let selectedVideo = videoDetails.find((video) => video.id === id);
+
+    this.setState({
+      videoDetails: selectedVideo,
+    });
+  };
+
+  render() {
+    const filteredVideos = this.state.videoList.filter(
+      (video) => video.id !== this.state.videoDetails.id
+    );
+    return (
+      <div>
+        <MainVideo videos={this.state.videoDetails} />
+        <VideoInfo videos={this.state.videoDetails} />
+        <VideoDescription videos={this.state.videoDetails} />
+        <CommentsForm comments={this.state.videoDetails.comments} />
+        {/* <Comment comments={this.state.videoDetails} /> */}
+        <CommentsList comments={this.state.videoDetails.comments} />
+
+        {/* <SideVideos
+            sideVideos={this.selectedVideo}
+            eventHandler={this.updateSelectedVideo}
+        /> */}
+        <SideVideosList
+          list={filteredVideos}
+          eventHandler={this.updateSelectedVideo}
+        />
+      </div>
+    );
+  }
+}
+
+export default Home;
