dd46e3d728646f8205892531605c4d592a7aae74
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-06 02:01:10 -0800

    restarted styling info section

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -9,23 +9,30 @@ function VideoInfo(videoInfo) {
   return (
     <div className="video">
       <h1 className="video__title">{videoInfo.videoInfo.title}</h1>
+
+
       <div className="video__info">
         <div className="video__info__container">
-          <p className="video__info__channel">
+          <h4 className="video__info__channel">
             By {videoInfo.videoInfo.channel}
+          </h4>
+          <p className="video__info__text">
+            {videoDate.toLocaleDateString("en-US")}
           </p>
-          <div className="video__info__container__views">
-            <img src={views} alt="views" />
-            <p>{videoInfo.videoInfo.views}</p>
-          </div>
         </div>
-        <div className="video__info__container--margin">
-          <p>{videoDate.toLocaleDateString("en-US")}</p>
-          <div className="video__info__container__likes">
-            <img src={likes} alt="likes" />
-            <p>{videoInfo.videoInfo.likes}</p>
+
+          <div className="video__info__container">
+            <div className="video__info__container__views">
+              <img src={views} alt="views" className="video__info__icons" />
+              <p className="video__info__text video__info__text--tablet">
+                {videoInfo.videoInfo.views}
+              </p>
+            </div>
+            <div className="video__info__container__likes">
+              <img src={likes} alt="likes" className="video__info__icons" />
+              <p className="video__info__text">{videoInfo.videoInfo.likes}</p>
+            </div>
           </div>
-        </div>
       </div>
     </div>
   );
