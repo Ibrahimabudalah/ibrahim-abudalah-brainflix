2bc498887e70d5bad8c1fe18db415aa90ff2921d
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-06 02:51:58 -0800

    updated tablet styles (not finished)

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -3,37 +3,31 @@ import "./VideoInfo.scss";
 import views from "../../assets/icons/views.svg";
 import likes from "../../assets/icons/likes.svg";
 
-function VideoInfo(videoInfo) {
-  const videoDate = new Date(parseInt(videoInfo.videoInfo.timestamp));
+function VideoInfo({ videoInfo }) {
+  const videoDate = new Date(parseInt(videoInfo.timestamp));
   // console.log(videos);
   return (
     <div className="video">
-      <h1 className="video__title">{videoInfo.videoInfo.title}</h1>
-
-
-      <div className="video__info">
-        <div className="video__info__container">
-          <h4 className="video__info__channel">
-            By {videoInfo.videoInfo.channel}
-          </h4>
+      <h1 className="video__title">{videoInfo.title}</h1>
+      <section className="video__info">
+        <div className="video__info__component-one">
+          <h4 className="video__channel">By {videoInfo.channel}</h4>
           <p className="video__info__text">
             {videoDate.toLocaleDateString("en-US")}
           </p>
         </div>
 
-          <div className="video__info__container">
-            <div className="video__info__container__views">
-              <img src={views} alt="views" className="video__info__icons" />
-              <p className="video__info__text video__info__text--tablet">
-                {videoInfo.videoInfo.views}
-              </p>
-            </div>
-            <div className="video__info__container__likes">
-              <img src={likes} alt="likes" className="video__info__icons" />
-              <p className="video__info__text">{videoInfo.videoInfo.likes}</p>
-            </div>
+        <div className="video__info__component-two">
+          <div className="video__info__component__views">
+            <img src={views} alt="views" className="video__info__icons" />
+            <p>{videoInfo.views}</p>
           </div>
-      </div>
+          <div className="video__info__component__likes">
+            <img src={likes} alt="likes" className="video__info__icons" />
+            <p>{videoInfo.likes}</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
