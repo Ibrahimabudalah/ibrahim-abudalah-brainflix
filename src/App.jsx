2749ba8f1b4055647e3160299905fb27f68feb3d
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-06 20:58:07 -0800

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "../src/pages/Home/Home";
 import Upload from "./pages/Upload/Upload";
-
+ 
 class App extends React.Component {
   render() {
     return (
