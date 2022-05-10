1697b61c1e9d8a7eebdcec9fc9ac59f25677e699
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-05-09 20:10:37 -0700

    ready for deployment2

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-const BASE_URL = "https://brainflix-apii.herokuapp.com/";
+// const BASE_URL = "https://brainflix-apii.herokuapp.com/";
 // const API_KEY = "2596369b-58ce-414f-bde8-c1ee6434cbda";
 
-export default {
-  getAll: () => axios.get("https://brainflix-apii.herokuapp.com/videos"),
+// export default {
+   const getAll = () => { return axios.get("https://brainflix-apii.herokuapp.com/videos")}
   //this method will be used to get all the data from the api using the key
-  getVideoById: (id) =>
-    axios.get(`${BASE_URL}videos/${id}`),
+  const getVideoById = (id) =>{ return
+    axios.get(`https://brainflix-apii.herokuapp.com/videos/${id}`)}
   //   //this method will be used to get a specific video from the api using its "id"
-};
+// };
+const exportObject = {
+  getAll,
+  getVideoById
+}
+export default exportObject
