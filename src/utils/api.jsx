dbe76aebfab5e8de1d76b1e75dc31edc40bce900
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-01-29 02:11:48 -0800

    changed the api URL

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "https://brainflix-apii.herokuapp.com/";
+const BASE_URL = "https://morning-shelf-42466.herokuapp.com/";
 // const API_KEY = "2596369b-58ce-414f-bde8-c1ee6434cbda";
 
 const apiUtils = {
-  getAll: () => axios.get("https://brainflix-apii.herokuapp.com/videos"),
+  getAll: () => axios.get("https://morning-shelf-42466.herokuapp.com/videos"),
   //this method will be used to get all the data from the api using the key
-  getVideoById: (id) =>
-    axios.get(`${BASE_URL}videos/${id}`),
+  getVideoById: (id) => axios.get(`${BASE_URL}videos/${id}`),
   //   //this method will be used to get a specific video from the api using its "id"
 };
 export default apiUtils;
