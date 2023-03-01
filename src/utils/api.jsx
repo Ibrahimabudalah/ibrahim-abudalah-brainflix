c1d7138178879faeda58c359b01b9d2d6e0db25e
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-03-01 00:38:50 -0800

    updated server url

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://morning-shelf-42466.herokuapp.com/";
+const BASE_URL = "https://brainflix-api-1233.onrender.com/";
 // const API_KEY = "2596369b-58ce-414f-bde8-c1ee6434cbda";
 
 const apiUtils = {
-  getAll: () => axios.get("https://morning-shelf-42466.herokuapp.com/videos"),
+  getAll: () => axios.get(`${BASE_URL}videos`),
   //this method will be used to get all the data from the api using the key
   getVideoById: (id) => axios.get(`${BASE_URL}videos/${id}`),
   //   //this method will be used to get a specific video from the api using its "id"
